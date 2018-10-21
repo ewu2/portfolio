b752d774c44d11723b716e2cc41b550b4c5d8ade
ewu <es.nj.wu@gmail.com>
2018-10-20 23:16:53 -0700

    made major updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    //fade in main elements
+    $('.main').fadeIn(3000);
+
+    //fade in about me elements
+    $('.about-me').fadeIn(3000);
+
+    //fade in code elements
+    $('.col1').fadeIn(1000);
+    $('.col2').fadeIn(2000);
+    $('.col3').fadeIn(3000);
+    $('.col4').fadeIn(4000);
+    $('.col5').fadeIn(5000);
+    $('.col6').fadeIn(6000);
+
+    //fade in video elements
+    $('.vid1').fadeIn(4000);
+    $('.vid2').fadeIn(5000);
+    $('.vid3').fadeIn(6000);
+    $('.vid4').fadeIn(7000);
+    $('.vid5').fadeIn(8000);
+    $('.vid6').fadeIn(9000);
+    $('.header1').fadeIn(4000);
+    $('.header2').fadeIn(5000);
+    $('.header3').fadeIn(6000);
+    $('.header4').fadeIn(7000);
+    $('.header5').fadeIn(8000);
+    $('.header6').fadeIn(9000);
+})
